1145f05348b678cb29c8e89fc365037bee48894d
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-09-07 14:44:16 +0600

    use variable

--- local.tf
+++ local.tf
@@ -1,26 +1,31 @@
-terraform {
-    required_providers{
-        docker = {
-            source = "kreuzwerker/docker"
-            version = "~> 2.21.0"
-        }
-    }
-}
+# terraform {
+#     required_providers{
+#         docker = {
+#             source = "kreuzwerker/docker"
+#             version = "~> 2.21.0"
+#         }
+#     }
+# }
 
-provider "docker" {}
+# provider "docker" {}
 
 
-resource "docker_image" "nginx" {
-    name = "nginx:latest"
-    keep_locally = false
-}
+# resource "docker_image" "nginx" {
+#     name = "nginx:latest"
+#     keep_locally = false
+# }
 
-resource "docker_container" "nginx"{
-    image = docker_image.nginx.image_id
-    name = "nginx-custom-tf-by-ismail"
+# resource "docker_container" "nginx"{
+#     image = docker_image.nginx.image_id
+#     name = "nginx-custom-tf-by-ismail"
 
-    ports{
-        internal = 80
-        external = 80
-    }
+#     ports{
+#         internal = 80
+#         external = 80
+#     }
+# }
+
+resource "local_file" "aktaname" {
+    filename = var.filename 
+    content = var.content
 }
